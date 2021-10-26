a125f18d483f7adfb7bf3a2790ba048d2e58080c
Yanni App13 <jonvincruman@gmail.com>
2021-10-26 05:20:25 -0700

    first display three messages

---
+++ chapter1/programmingExercises/1_1_displayThreeMessages/displayThreeMessages.java
@@ -0,0 +1,12 @@
+// Write a program that displays:
+//     Welcome to Java
+//     Welcome to Computer Science
+//     Programming is fun
+
+public class displayThreeMessages {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Computer Science");
+        System.out.println("Programming is fun");
+    }
+}
