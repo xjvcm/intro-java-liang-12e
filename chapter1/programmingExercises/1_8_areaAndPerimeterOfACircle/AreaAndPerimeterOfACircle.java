60002be1de618bee45abbe45dcc94b94721d4fce
Yanni App13 <jonvincruman@gmail.com>
2021-11-05 04:43:50 -0700

    area and perimeter of a circle

---
+++ chapter1/programmingExercises/1_8_areaAndPerimeterOfACircle/AreaAndPerimeterOfACircle.java
@@ -0,0 +1,12 @@
+public class AreaAndPerimeterOfACircle {
+
+  public static void main(String[] args) {
+
+    double rad = 5.5;
+    System.out.println("Radius = 5.5");
+    System.out.print("perimeter = ");
+    System.out.println(2 * rad * Math.PI);
+    System.out.print("area = ");
+    System.out.println(rad * rad * Math.PI);
+  }
+}
