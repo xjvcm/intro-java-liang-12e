1c96db9a27d3e38380be3abb1f1a990e61a7927e
Yanni App13 <jonvincruman@gmail.com>
2021-11-05 04:50:08 -0700

    finished area and perimeter for rectangle

---
+++ chapter1/programmingExercises/1_9_areaAndPerimeterOfARectangle/AreaAndPerimeterOfARectangle.java
@@ -0,0 +1,14 @@
+public class AreaAndPerimeterOfARectangle {
+
+    public static void main(String[] args) {
+
+        double width = 4.5;
+        double height = 7.9;
+
+        System.out.println("Rectangle with width = 4.5 and height = 7.9");
+        System.out.print("perimeter = ");
+        System.out.println((2 * width) + (2 * height));
+        System.out.print("area = ");
+        System.out.println(width * height);
+    }
+}
